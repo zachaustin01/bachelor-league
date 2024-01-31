b878954c7412f7d74977fd69277347d6d221659d
Zach Austin <76120216+zachaustin01@users.noreply.github.com>
2024-01-30 21:02:15 -0600

    Add contest rules

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,12 +2,40 @@
 
 import React from 'react';
 
+class ContestRules extends React.Component {
+    render() {
+      const rules = [
+        '🌹 Rose received: (1)',
+        '✨ First / Last rose in ceremony: (1)',
+        '👥 Group date rose',
+        '😏 Starts a rumor and receives rose in same episode: (1)',
+        '😞 Starts a  rumor and goes home in same episode: (-1)',
+        '💪 Starts a rumor about another girl and that girl goes home same episode (1)',
+        '💰 "Steals" Joey from another girl and gets a rose',
+        '🚨 "Steals" Joey from another girl and goes home same episode',
+        '💍 Receives proposal',
+        '👗 Accepts proposal',
+        '😢 Rejects proposal',
+    ];
+
+      return (
+        <div>
+          <ul className="no-bullets">
+            {rules.map((rule, index) => (
+              <li key={index} className="glow-on-hover">{rule}</li>
+            ))}
+          </ul>
+        </div>
+      );
+    }
+  }
+
 function HomePage(league) {
     console.log(league)
     return (
         <div>
-        <h1>Home Page</h1>
-        <p>Welcome to the Home Page</p>
+        <h1>Contest Rules</h1>
+        <ContestRules/>
         </div>
     );
 }
